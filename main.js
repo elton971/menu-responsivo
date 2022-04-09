fccda7b2417975712a21d2ad44f73a0a58bde7d1
Elton Cavele <eltoncavele8@gmail.com>
2022-04-09 19:58:10 +0200

    new

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 
-
-    function fazGet(url) {
-        let request = new XMLHttpRequest();
-        request.open("GET", url, false);
-        //request.send();
-        return request.responseText;
-    }
-
-    // const tratamento = () => {
-
-    //     let data = fazGet("https://blog-api-mz.herokuapp.com/api/v1");
-    //     let resultado = JSON.parse(data);
-
-    //     console.log(resutado)
-
-    // }
-
     var content = document.getElementById("content");
     var imagem = document.getElementById("imagem");
     var titulo = document.getElementById("titulo");
@@ -26,27 +9,28 @@
     const tratamento = async() => {
 
             const url = `https://blog-api-mz.herokuapp.com/api/v1/blog/posts`;
-            const dados = await fetch(url);
-            const {posts} = await dados.json();
-            posts.forEach(element => {
-                listagemento(element);
-
-            });    
+            fetch(url).then(response => response.json()).then(data => {
+                data.posts.forEach(element => {
+                    listagemento(element);
+                });
+            
+            });
     }
     
     const listagemento =(res) => {
 
         main.innerHTML += `
-        <div class="titulo" id="titulo">
-            <h1>`+ res.title+`<h1>
-        </div>
-        <div class="main__conteudo">
-            <div class="content" id="content">
-
-                <p>`+res.content+`<p>
+        <div class="main__content">
+            <div class="titulo" id="titulo">
+                <h1>`+ res.title+`<h1>
             </div>
-            <div class="imagem" id="imagem">
-                 <img src="${res.imgUrl}" alt="">
+            <div class="main__conteudo__info">
+                <div class="content" id="content">
+                    <p>`+res.content+`<p>
+                </div>
+                <div class="imagem" id="imagem">
+                    <img src="${res.imgUrl}" alt="">
+                </div>
             </div>
         </div>`                 
     };
